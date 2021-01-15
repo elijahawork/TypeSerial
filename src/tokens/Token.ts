6b327070749fb388a48ae77d28055d1d2cd9b793
Eli Simkin <es340716@g.risd.org>
2021-01-14 22:47:49 -0600

    Created the Token module

---
+++ src/tokens/Token.ts
@@ -0,0 +1,72 @@
+export enum TokenTypes {
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    MOD,
+    ASSIGN,
+    NOT,
+    EQUAL,
+    LESSER,
+    GREATER,
+    LEQUAL,
+    GEQUAL,
+    AND,
+    OR,
+    
+    OPENPAR,
+    CLOSEPAR,
+    OPENBRACK,
+    CLOSEBRACK,
+    OPENBRACE,
+    CLOSEBRACE,
+    QUOTE,
+    APOSTROPHE,
+    COMMA,
+    DOT,
+    COLON,
+    SEMICOLON,
+
+    INT,
+    FLOAT,
+    BOOL,
+    CHAR,
+    STR,
+
+    KWORD,
+    SYMBOL,
+}
+export abstract class Token {
+    public readonly type: number;
+    public readonly line: number;
+
+    constructor(type: number, line: number) {
+        this.type = type;
+        this.line = line;
+    }
+}
+export abstract class Operator extends Token { }
+export class ArithmeticOperator extends Operator { }
+export class LogicalOperator extends Operator { }
+export class AssignmentOperator extends Operator { }
+
+export class Separator extends Token { }
+
+export abstract class GenericToken extends Token {
+    public readonly value: string;
+
+    constructor(type: number, line: number, value: string) {
+        super(type, line);
+        this.value = value;
+    }
+}
+export class Literal extends GenericToken {
+    constructor(type: number, line: number, value: string) {
+        super(type, line, value);
+    }
+}
+export class Identifier extends GenericToken {
+    constructor(type: number, line: number, value: string) {
+        super(type, line, value);
+    }
+}
