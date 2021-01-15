1392a524998c01d4adf4bfa60b28f9d13d31a0d9
Elijah Simkin <elijah.a.work@gmail.com>
2021-01-15 10:49:18 -0600

    Set up basic unit testing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,15 @@ import { LexerFactory } from './tokens/LexerFactory';
 sourceMapSupport.install();
 
 (function main() {
-    const input = 'int main(string[] args) { };';
+    const input = 
+    `fn int main(string[] args) 
+    {
+        let i = 5 + 2 * 3 / 4 % 7 - 1;
+        if (i < 120 + 2) {
+            print("hello world,");
+        }
+        
+    };`;
     const lexer = LexerFactory(input);
 
     const tokens = lexer.tokens.map(e => e.toAnonymousDebuggingToken());
