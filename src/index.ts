b283d93e8aef3aabecbc888709b4d8e9def91ae1
Elijah Simkin <elijah.a.work@gmail.com>
2021-01-15 09:44:23 -0600

    Added testing options

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 // provides TypeScript stack errors
 import sourceMapSupport from 'source-map-support';
+import util from 'util';
+import { LexerFactory } from './tokens/LexerFactory';
 sourceMapSupport.install();
 
 (function main() {
-    throw new Error('Hello, world');
+    const input = 'int main(string[] args) { };';
+    const lexer = LexerFactory(input);
+
+    const tokens = lexer.tokens.map(e => e.toAnonymousDebuggingToken());
+
+    console.log(util.inspect(tokens, false, null, true));
 })();
